83dc5eb63a0a959d6586455d0fa86b3ff51ddb78
Mathias <17691448+RespectMathias@users.noreply.github.com>
2023-05-09 22:00:20 +0200

    Apply gitignore

---
+++ VLSMistique/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿/* Default WPF Window template provided by Visual Studio or other IDEs. */
+
+using System.Windows;
+
+namespace VLSMistique
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ VLSMistique/Models/SubnetModel.cs
@@ -0,0 +1,59 @@
+﻿/*   Copyright 2023 Mathias Lund-Hansen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License. */
+
+using CommunityToolkit.Mvvm.ComponentModel;
+using System.Net;
+using VLSMistique.Interfaces;
+
+namespace VLSMistique.Models
+{
+    /// <summary> The class that represents a subnet. </summary>
+    public partial class SubnetModel : ObservableObject
+    {
+        /// <summary> The required amount of hosts of the subnet. </summary>
+        [ObservableProperty]
+        public int _hostAmount;
+
+        /// <summary> The Broadcast Address of the subnet. </summary>
+        [ObservableProperty]
+        public IPAddress _broadcastAddress;
+
+        /// <summary> The Network Address of the subnet. </summary>
+        [ObservableProperty]
+        public IPAddress _networkAddress;
+
+        /// <summary> The Subnet Mask of the subnet. </summary>
+        [ObservableProperty]
+        public string _mask;
+
+        /// <summary> The IP-range of the subnet. Meaning the first and last IP-address of the subnet. </summary>
+        [ObservableProperty]
+        public string _range;
+
+        /// <summary> The maximum amount of hosts in the subnet. </summary>
+        [ObservableProperty]
+        public int _maxSubnetHosts;
+
+        /// <summary> Contains the values of the subnet. </summary>
+        public SubnetModel(IPAddress broadcastAddress, IPAddress networkAddress, string mask, string range, int hostAmount, int maxSubnetHosts)
+        {
+            BroadcastAddress = broadcastAddress;
+            NetworkAddress = networkAddress;
+            Mask = mask;
+            Range = range;
+            HostAmount = hostAmount;
+            MaxSubnetHosts = maxSubnetHosts;
+        }
+    }
+}
